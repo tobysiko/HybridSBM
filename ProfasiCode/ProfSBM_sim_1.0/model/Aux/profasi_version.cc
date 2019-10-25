b29875bf9235162a4c8f8f7ca5e0a0734e3b9be0
Tobias <t.sikosek@utoronto.ca>
2019-10-25 23:06:35 +0200

    cleanup

--- ProfasiCode/ProfSBM_sim_1.0/model/Aux/profasi_version.cc
+++ ProfasiCode/ProfSBM_sim_1.0/model/Aux/profasi_version.cc
@@ -1,3 +1,3 @@
 #include "profasi_version.hh"
 std::string profasi_version() { return "1.4.9"; }
-std::string profasi_git_version() { return "git:"; }
+std::string profasi_git_version() { return "git:0ff73db699ba4151861727725587dbd3e129c6eb"; }
